b2b5b3769f6b8f5ed87a3982cb79f4e21e227033
Pete Saia <pete@petesaia.com>
2014-05-11 11:03:20 -0300

    started adding http mocks

--- src/service/spreadsheet.js
+++ src/service/spreadsheet.js
@@ -1,4 +1,5 @@
-oleo.service('spreadsheetService', function($http) {
+// https://developers.google.com/google-apps/spreadsheets/
+oleo.service('spreadsheetService', function($http, $q, $rootScope) {
 
   // Parse a google spreadsheet url and return a "url object".
   // Returns false if not a propery spreadsheet url.
@@ -6,13 +7,12 @@ oleo.service('spreadsheetService', function($http) {
   this.parseUrl = function(url) {
     var matches = [],
         urlObj = {};
-
     if (typeof url !== "string") {
-      return false;
+      throw new Error("Invalid url.");
     }
     matches = url.match(/^https:\/\/.*google.com.*\/d\/([^/]+).*$/i);
     if (!matches || matches.length < 2) {
-      return false;
+      throw new Error("Invalid url.");
     }
     urlObj.key = matches[1];
     matches = url.match(/gid=([^/]+)/i);
@@ -23,7 +23,31 @@ oleo.service('spreadsheetService', function($http) {
     }
     return urlObj;
   };
-  this.cells = function(urlObject) {
-    var endpoint = "https://spreadsheets.google.com/feeds/cells/%key%/%worksheet%/public/basic?alt=json-in-script&callback=";
+
+  this.cellsEndpoint = function(urlObj) {
+    return "https://spreadsheets.google.com/feeds/cells/"+
+        urlObj.key+"/"+urlObj.worksheet+"/public/basic"+
+        "?alt=json-in-script&callback=JSON_CALLBACK";
+  };
+
+  // Request and parse all cell data for a sheet. This will return
+  // a promise.
+  this.cells = function(urlObj) {
+    var deferred = $q.defer();
+    var promise = deferred.promise;
+    var collection = [];
+    var onSuccess = function(res, status) {
+      var feed = res.feed;
+      var entries = feed.entry || [];
+      deferred.resolve(res, status);
+    };
+    var onError = function(res, status) {
+      deferred.reject(new Error("Invalid response: "+res));
+    };
+    $http({
+      url: this.cellsEndpoint(urlObj),
+      method: "JSONP"
+    }).success(onSuccess).error(onError);
+    return promise;
   };
 });
--- test/test.services.js
+++ test/test.services.js
@@ -1,34 +1,78 @@
 var expect = chai.expect;
 var assert = chai.assert;
 
+// Basic ajax loader for dummy content.
+var load = function(url, cb) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = loaded;
+    httpRequest.open('GET', url);
+    httpRequest.send();
+    function loaded() {
+      if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+          cb(httpRequest.responseText);
+        } else {
+          throw new Error("Problem loading content: ", httpRequest);
+        }
+      }
+    }
+};
+
 describe("services", function() {
   beforeEach(module('oleo'));
   describe("spreadsheets", function() {
 
     var spreadsheet;
+    var withWorksheet;
+    var noWorksheet;
+    var $httpBackend;
 
-    beforeEach(inject(function(spreadsheetService) {
+    // Below is a live & public spreadsheet key.
+    var SAMPLE_KEY = "15lLlaf9DdGr-4SY8n8saxkwB-8Yvd7OcKQhgL5BFaY4";
+
+    beforeEach(inject(function(spreadsheetService, $injector) {
+      $httpBackend = $injector.get("$httpBackend");
       spreadsheet = spreadsheetService;
+      withWorksheet = "https://docs.google.com/a/lev-interactive.com/spreadsheets/d/15lLlaf9DdGr-4SkwB-8Yvd7OcKQ4/edit#gid=123456";
+      noWorksheet = "https://docs.google.com/a/lev-interactive.com/spreadsheets/d/15lLlaaxkwB-8Yvd7OcKL5BFaY4/edit";
     }));
 
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
     it("should properly parse a google spreadsheet url", function() {
-      var withWorksheet = "https://docs.google.com/a/lev-interactive.com/spreadsheets/d/15lLlaf9DdGr-4SkwB-8Yvd7OcKQ4/edit#gid=123456";
-      var noWorksheet = "https://docs.google.com/a/lev-interactive.com/spreadsheets/d/15lLlaaxkwB-8Yvd7OcKL5BFaY4/edit";
       var justBad = "http://blah";
-
-      var r = expect(spreadsheet.parseUrl(justBad)).to.be.false;
-
+      var badFunc = function() {
+        spreadsheet.parseUrl(justBad);
+      };
+      var r = expect(badFunc).to.throw(Error, "Invalid url.");
       r = expect(spreadsheet.parseUrl(withWorksheet)).to.deep.equal({
         key: "15lLlaf9DdGr-4SkwB-8Yvd7OcKQ4",
         worksheet: "123456"
       }, "parsed out the worksheet gid");
-
       r = expect(spreadsheet.parseUrl(noWorksheet)).to.deep.equal({
         key: "15lLlaaxkwB-8Yvd7OcKL5BFaY4",
         worksheet: "od6"
       }, "worksheet defaults to od6.. the first.");
     });
-    it("should retrieve a basic spreadsheet from google (real http request)", function() {});
+
+    it("should properly parse data from google", function(done) {
+      var urlObj = { key: "abc", worksheet: "123" };
+      var url = spreadsheet.cellsEndpoint(urlObj);
+
+      load("./dummy/cells.json", function(content) {
+        $httpBackend.whenJSONP(url).respond(200, JSON.parse(content));
+        spreadsheet.cells(urlObj).then(function(res) {
+          console.log(res);
+          done();
+        }, function(err) {
+          throw err;
+        });
+        $httpBackend.flush();
+      });
+    });
     it("should be able to write to a google spreadsheet", function() {});
     it("should be able to fetch a specifc row by id", function() {});
     it("should be able fetch the spreadsheet's title", function() {});
