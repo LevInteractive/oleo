040026ed6a57267426f2e94341f512b2e4a1bf96
Pete Saia <pete@petesaia.com>
2014-05-13 09:18:34 -0300

    added prototype to project model

--- src/factory/project.js
+++ src/factory/project.js
@@ -1,10 +1,14 @@
 oleo.factory("Project", function() {
   function Project(opts) {
-    this.id = Math.random().toString(36).substring(10);
-    this.name = opts.name;
+    this.id = opts.id || Math.random().toString(36).substring(10);
+    this.name = opts.name || "";
     this.weight = opts.weight || 0;
     this.creationDate = new Date();
+    this.active = false;
   }
+  Project.prototype.isActive = function() {
+    return this.active;
+  };
   Project.build = function(obj) {
     return new Project(obj);
   };
