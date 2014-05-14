452a7a31941b83bb2a1b90dd1fdb34d2c0543b21
Pete Saia <pete@petesaia.com>
2014-05-14 08:42:53 -0300

    cleaned up unit test a little

--- test/test.project.js
+++ test/test.project.js
@@ -3,19 +3,17 @@ var assert = chai.assert;
 
 describe("project model", function() {
   beforeEach(module('oleo'));
-  var $httpBackend;
-  var $injector;
+
   var ProjectFactory;
 
-  beforeEach(inject(function(Project, _$injector_) {
-    $injector = _$injector_;
+  beforeEach(inject(function(Project) {
     ProjectFactory = Project;
   }));
 
   it("should be able to create thousands of unqiue ids", function() {
     var i = 10000, c = {}, _i = i;
     while(i--) {
-      c[(new ProjectFactory()).get("id")] = i;
+      c[new ProjectFactory().get("id")] = i;
     }
     expect(Object.keys(c)).to.have.length(_i);
   });
